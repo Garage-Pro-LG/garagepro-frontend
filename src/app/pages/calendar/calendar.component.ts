8b8c292aef137eb7bf6563dcf415aa385f1ba218
Elisenda-LV <elisenda.lv@gmail.com>
2025-02-08 23:39:22 +0100

    full calendar installed and config

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [ FullCalendarModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
 
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, bootstrapPlugin, interactionPlugin, timeGridPlugin, listPlugin],
+    firstDay: 1,
+    // themeSystem: 'bootstrap',
+    headerToolbar: {
+      left: 'title',
+      center: 'dayGridMonth,listWeek',
+      right: 'prev,today,next'
+    },
+    buttonText: {
+      today: 'avui',
+      month: 'mes',
+      list: 'llista'
+    }
+  };
 }
