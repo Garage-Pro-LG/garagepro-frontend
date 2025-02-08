9ecf4759685e5bcecb37b0a698c2a5673ac20451
Elisenda-LV <elisenda.lv@gmail.com>
2025-02-08 19:33:51 +0100

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    title: 'gp',
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+
+  },
+ /*  {
+    path: 'calendar',
+    title: 'gp - calendar',
+    loadComponent: () => import('./pages/calendar/calendar.component').then(c => c.CalendarComponent),
+
+  }, */
+  {
+    path: '**',
+    title: 'something went wrong',
+    loadComponent: () => import('./pages/error/error.component').then(c => c.ErrorComponent),
+
+  },
+
+];
